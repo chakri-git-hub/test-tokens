5131f22a70d601fa7eda04a6f513b66066b5d000
Chakrimaidam <m.chakri906@gmail.com>
2024-08-22 17:15:49 +0530

    scss files commit

---
+++ tokens/src/App.js
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="chakri">chakri</h1>
+      <button className="button-danger">Delete</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tokens/webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  mode: 'production',
+};
